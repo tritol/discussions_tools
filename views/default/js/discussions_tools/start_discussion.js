39753bc5b962c40327be06ab485c2d77785babc2
Jerome Bakker <jeabakker@coldtrick.com>
2016-03-22 14:35:51 +0100

    fixed: using correct jquery function

--- views/default/js/discussions_tools/start_discussion.js
+++ views/default/js/discussions_tools/start_discussion.js
@@ -3,10 +3,10 @@ define(['jquery', 'elgg'], function ($, elgg) {
 	$(document).on('submit', '#discussions-tools-start-discussion-widget-form', function () {
 		var selected_group = $('#discussions-tools-discussion-quick-start-group').val();
 		if (selected_group !== '0') {
-			$('#discussions-tools-discussion-quick-start-access_id option').removeAttr('selected');
+			$('#discussions-tools-discussion-quick-start-access_id option').prop('selected', false);
 			$('#discussions-tools-discussion-quick-start-access_id option').each(function (index, elem) {
 				if ($(elem).html() === selected_group) {
-					$(elem).attr('selected', 'selected');
+					$(elem).prop('selected', true);
 				}
 			});
 		} else {
