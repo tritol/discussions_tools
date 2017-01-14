07a23f7d546bb1eacd2807eaba90d6b96f58776a
Lubomír Bláha <tritol@trilogic.cz>
2017-01-14 23:01:44 +0100

    Czech translation

---
+++ languages/cs.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+	
+	// group tool option descriptions
+	'forum:group_tool_option:description' => "Povolit členům skupiny zahájit diskusi ve formátu jednoduchého fóra.",
+
+	// discussion status
+	'discussions_tools:discussion:confirm:open' => "Jste si jistý/á, že chcete znovu otevřít toto téma?",
+	'discussions_tools:discussion:confirm:close' => "Jste si jistý/á, že chcete uzavřít toto téma?",
+	
+	// widgets
+	// quick start discussion
+	'discussions_tools:widgets:start_discussion:title' => "Zahájit diskusi",
+	'discussions_tools:widgets:start_discussion:description' => "Rychlé zahájení diskuse ve zvolené skupině",
+	
+	'discussions_tools:widgets:start_discussion:login_required' => "K použití tohoto udělátka musíte být přihlášen/a",
+	'discussions_tools:widgets:start_discussion:membership_required' => "K použití tohoto udělátka musíte být členem alespoň jedné skupiny. Zajímavé skupiny můžete najít %stady%s.",
+	'discussions_tools:widgets:start_discussion:not_enabled' => "Žádná z vašich skupin nemá povolené diskuse.",
+	
+	'discussions_tools:forms:discussion:quick_start:group' => "Zvolte skupinu pro tuto diskusi",
+	'discussions_tools:forms:discussion:quick_start:group:required' => "Zvolte prosím skupinu",
+	
+	// latest discussions (index, dashboard)
+	'discussions_tools:widgets:discussion:description' => "Zobrazuje nejnovější diskuse",
+	'discussions_tools:widgets:discussion:settings:group_only' => "Pouze diskuse ze skupin kterých jste členem",
+	'discussions_tools:widgets:discussion:more' => "Zobrazit více diskusí",
+	
+	// latest discussions (group)
+	'discussions_tools:widgets:group_forum_topics:description' => "Zobrazuje nejnovější diskuse",
+	
+	// actions
+	// discussion toggle status
+	'discussions_tools:action:discussions:toggle_status:success:open' => "Téma bylo úspěšně otevřeno",
+	'discussions_tools:action:discussions:toggle_status:success:close' => "Téma bylo úspěšně uzavřeno",
+	
+];
